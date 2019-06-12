c7262b7f55d250a328141c4c4cafcd5be9210c4e
Jack Maloney <jmmaloney4@gmail.com>
2019-06-11 20:13:38 -0500

    Add PathKit+Codable.swift

---
+++ Sources/Villa/PathKit+Codable.swift
@@ -0,0 +1,22 @@
+//
+//  PathKit+Codable.swift
+//  Villa
+//
+//  Created by Jack Maloney on 6/11/19.
+//
+
+import Foundation
+import PathKit
+
+extension Path: Codable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let raw = try container.decode(String.self)
+        self.init(raw)
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(self.normalize().path)
+    }
+}
